0b024a6c5b9288fe30f512a54eae7364fb320995
Sky Albert <trantoan.fox.97@gmail.com>
2019-09-24 09:50:16 +0700

    [PhotoLibrary] Make API - not test

--- server/src/routes/modules/photolibrary.route.js
+++ server/src/routes/modules/photolibrary.route.js
@@ -6,18 +6,15 @@
  */
 const router = require( "express-promise-router" )();
 
-const PostServerController = require( "../../controllers/postserver.controller" );
+const PhotoLibraryController = require( "../../controllers/photolibrary.controller" );
 const auth = require( "../../helpers/middleware/authenticate.middleware" );
 const permission = require( "../../helpers/middleware/permission.middleware" );
 
 router
   .route( "/" )
-  .get( auth, permission, PostServerController.index )
-  .post( auth, permission, PostServerController.create )
-  .patch( auth, permission, PostServerController.update )
-  .delete( auth, permission, PostServerController.delete );
-
-router.route( "/:uid/status" ).post( PostServerController.changeStatus );
-router.route( "/online" ).post( PostServerController.getServerOnline );
+  .get( auth, permission, PhotoLibraryController.index )
+  .post( auth, permission, PhotoLibraryController.create )
+  .patch( auth, permission, PhotoLibraryController.update )
+  .delete( auth, permission, PhotoLibraryController.delete );
 
 module.exports = router;
